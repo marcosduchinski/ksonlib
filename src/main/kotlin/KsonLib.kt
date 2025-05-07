046bb144c8c53c21c6843f03c7bc58047ee43731
Marcos Viana Duchinski <mvid@tjpr.jus.br>
2025-05-07 12:03:08 +0100

    mapType using when instead of if-else

--- src/main/kotlin/KsonLib.kt
+++ src/main/kotlin/KsonLib.kt
@@ -12,48 +12,30 @@ class KsonLib(val obj: Any?) {
         return declaredMemberProperties.first { it.name == parameter.name }
     }
 
-
-    fun mapType(value: Any?): JsonValue {
-        if (value == null) {
-            return JsonNull()
-        } else if (value.javaClass.isEnum) {
-            return JsonString(value.toString())
-        } else if (value is Int) {
-            return JsonNumber(value)
-        } else if (value is Double) {
-            return JsonNumber(value)
-        } else if (value is String) {
-            return JsonString(value)
-        } else if (value is Boolean) {
-            return JsonBoolean(value)
-        } else if (value::class.isData) {
+    private fun mapType(value: Any?): JsonValue = when {
+        value == null -> JsonNull()
+        value.javaClass.isEnum -> JsonString(value.toString())
+        value is Int -> JsonNumber(value)
+        value is Double -> JsonNumber(value)
+        value is String -> JsonString(value)
+        value is Boolean -> JsonBoolean(value)
+        value is List<*> -> JsonArray(value.map {
+            mapType(it)
+        }.toMutableList())
+        value is Map<*, *> -> JsonObject(value.entries.associate {
+            (k, v) -> k.toString() to mapType(v)
+        }.toMutableMap())
+        value::class.isData -> {
             val clazz = value::class
-            val members = mutableMapOf<String, JsonValue>()
-            clazz.primaryConstructor?.parameters?.forEach { p ->
-                val prop = clazz.matchProperty(p)
-                val jsonValue = mapType(prop.call(value))
-                members[prop.name] = jsonValue
-            }
-            return JsonObject(members)
-        } else if (value is List<*>) {
-            val jsonArray = JsonArray(mutableListOf())
-            value.forEach { item ->
-                val jsonValue = mapType(item)
-                jsonArray.elements.add(jsonValue)
-            }
-            return jsonArray
-        } else if (value is Map<*, *>) {
-            val jsonObject = JsonObject(mutableMapOf())
-            value.forEach { (key, item) ->
-                val jsonValue = mapType(item)
-                jsonObject.members[key.toString()] = jsonValue
-            }
-            return jsonObject
-        } else {
-            TODO("Unsupported type: ${value::class.simpleName}")
+            val members = clazz.primaryConstructor?.parameters?.associate { param ->
+                val prop = clazz.matchProperty(param)
+                prop.name to mapType(prop.call(value))
+            } ?: emptyMap()
+            JsonObject(members.toMutableMap())
         }
-
+        else -> TODO("Unsupported type: ${value::class.simpleName}")
     }
+
 
     fun asJson(): String {
         return mapType(obj).asJson()
