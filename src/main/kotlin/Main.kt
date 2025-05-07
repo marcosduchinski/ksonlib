e1d4465a5233757e1ae9241fa36d685c5a752075
Marcos Viana Duchinski <mvid@tjpr.jus.br>
2025-05-07 13:04:23 +0100

    validation of empty list and empty map class Course

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,5 @@
-data class Course(val name: String, val credits: Int, val evaluation: List<EvalItem>)
-data class EvalItem(val name: String, val percentage: Double, val mandatory: Boolean, val type: EvalType?)
-enum class EvalType { TEST, PROJECT, EXAM }
-
 fun main() {
-    val course =
-        Course("PA", 6, listOf(EvalItem("quizzes", .2, false, null), EvalItem("project", .8, true, EvalType.PROJECT)))
-    val customerJson = KsonLib(course).asJson()
-    println()
-    println(customerJson)
+   print("Not implemented yet")
 }
 
 
--- src/test/kotlin/KsonLibTest.kt
+++ src/test/kotlin/KsonLibTest.kt
@@ -1,7 +1,11 @@
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class KsonLibTest {
+
+    data class Course(val name: String, val credits: Int, val evaluation: List<EvalItem>)
+    data class EvalItem(val name: String, val percentage: Double, val mandatory: Boolean, val type: EvalType?)
     enum class EvalType { TEST, PROJECT, EXAM }
 
 
@@ -41,6 +45,29 @@ class KsonLibTest {
         val value: EvalType = EvalType.TEST
         val expected = "\"${value.toString()}\""
         Assertions.assertEquals(expected, (KsonLib(value).asJson()))
+    }
+
+    @Test
+    fun should_throw_illegal_argument_exception_to_empty_list() {
+        val emptyList = emptyList<String>()
+        assertThrows<IllegalArgumentException>{
+            KsonLib(emptyList).asJson()
+        }
+    }
+
+    @Test
+    fun should_throw_illegal_argument_exception_to_empty_map() {
+        val emptyMap = emptyMap<String, String>()
+        assertThrows<IllegalArgumentException>{
+            KsonLib(emptyMap).asJson()
+        }
+    }
+
+    @Test
+    fun should_serialize_course () {
+        val course = Course("PA", 6, listOf(EvalItem("quizzes", .2, false, null), EvalItem("project", .8, true, EvalType.PROJECT)))
+        val expected = "{\"name\":\"PA\",\"credits\":6,\"evaluation\":[{\"name\":\"quizzes\",\"percentage\":0.2,\"mandatory\":false,\"type\":null},{\"name\":\"project\",\"percentage\":0.8,\"mandatory\":true,\"type\":\"PROJECT\"}]}"
+        Assertions.assertEquals(expected.replace("\\s+",""), KsonLib(course).asJson().replace("\\s+",""))
     }
 
 
