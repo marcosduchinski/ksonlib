ab86f6b9b29fc094e9885c8b50200855304212c5
Marcos Viana Duchinski <mvid@tjpr.jus.br>
2025-05-10 19:26:17 +0100

    delete files from phase 3

--- src/main/kotlin/annotations/Mapping.kt
+++
@@ -1,4 +0,0 @@
-package annotations
-
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
-annotation class Mapping(val name: String)
--- src/main/kotlin/annotations/Param.kt
+++
@@ -1,4 +0,0 @@
-package annotations
-
-@Target(AnnotationTarget.TYPE_PARAMETER, AnnotationTarget.VALUE_PARAMETER)
-annotation class Param()
