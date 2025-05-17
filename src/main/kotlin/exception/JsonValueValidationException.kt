f622ae4bbdbb95e059546bbafdca723fad0025c2
Marcos Viana Duchinski <mvid@tjpr.jus.br>
2025-05-17 12:52:10 +0100

    KDoc for CustomExceptions

--- src/main/kotlin/exception/JsonValueValidationException.kt
+++ src/main/kotlin/exception/JsonValueValidationException.kt
@@ -1,6 +1,19 @@
 package exception
 
+/**
+ * *Custom Exception*
+ *
+ * This class represents a custom exception that is thrown when is not possible to generate a valid JsonValue
+ *
+ * @property customMessage The custom message that describes the validation error.
+ */
 class JsonValueValidationException(val customMessage: String) : Exception() {
-    override val message: String?
+    override val message: String?/**
+     * Get the message of the exception.
+     *
+     * @return The message of the exception.
+     */
     get() = "JsonValue not valid: ${customMessage}"
 }
+
+//changed the signature
