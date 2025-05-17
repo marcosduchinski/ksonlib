198f3322b1d637ae55a5f3873970a1b7c8173712
Marcos Viana Duchinski <mvid@tjpr.jus.br>
2025-05-17 13:21:13 +0100

    Kdoc

--- src/main/kotlin/model/JsonValue.kt
+++ src/main/kotlin/model/JsonValue.kt
@@ -2,7 +2,30 @@ package model
 
 import visitor.JsonVisitor
 
+/**
+ * *Interface*
+ *
+ * This interface represents a JsonValue.
+ * Following the JSON built definition, available in https://www.json.org/json-en.html,
+ * a JsonValue can be:
+ * - A JsonObject
+ * - A JsonArray
+ * - A JsonString
+ * - A JsonNumber
+ * - A JsonBoolean
+ * - A JsonNull
+ */
 sealed interface JsonValue {
+    /**
+     * Convert the JsonValue to a JSON string representation.
+     *
+     * @return The JSON string representation of the JsonValue.
+     */
     fun asJson(): String
+    /**
+     * Accept a visitor to perform operations on the JsonValue.
+     *
+     * @param visitor The visitor to accept.
+     */
     fun accept(visitor: JsonVisitor)
 }
